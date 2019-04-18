21e420d151abe056be7699fe4e1788b997878ea7
Waldo Rabie <waldo.rabie@smartdata.net>
2019-04-17 23:43:44 -0400

    Finished front end development

---
+++ public/precache-manifest.92de5bfd600f81364d42f072f2bdde99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d02d921bc0ca128c86c30dd65d2ffc5c",
+    "url": "/static/media/logo.d02d921b.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "467f4716a84e7e9962d1",
+    "url": "/static/js/main.bf8b3e2d.chunk.js"
+  },
+  {
+    "revision": "7bfdb13267986d2c5c22",
+    "url": "/static/js/2.eb69afd3.chunk.js"
+  },
+  {
+    "revision": "467f4716a84e7e9962d1",
+    "url": "/static/css/main.04477e37.chunk.css"
+  },
+  {
+    "revision": "f43601761d883f94dd3cae39e972a65b",
+    "url": "/index.html"
+  }
+];
